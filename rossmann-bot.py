cc20d05bf17a0eac523280d708cd761ad6dae0fa
Matheus Guerreiro <Matheus-1095@users.noreply.github.com>
2022-12-12 19:54:57 -0300

    Eleventh Commit
    Forgot Os library

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -1,12 +1,11 @@
 import pandas as pd
+import os
 import json
 import requests
 from flask import Flask, request, Response
 
 # CONSTANTS
 TOKEN = '5824921864:AAFFW8yuir8632tnbntFqnXXvhIvsmYVNbQ'
-
-
 
 # # Info about the bot
 # 'https://api.telegram.org/bot5824921864:AAFFW8yuir8632tnbntFqnXXvhIvsmYVNbQ/getMe'
@@ -39,7 +38,7 @@ def load_dataset(store_id):
     df_test = pd.merge(df10, df_store, how='left', on='Store')
 
     # Choose store for prediction
-    df_test = df_test[df_test['Store'].isin([store_id])]
+    df_test = df_test[df_test['Store'] == store_id]
 
     if not df_test.empty:
         #remove closed days 
@@ -69,9 +68,9 @@ def predict(data):
 
 def parse_message(message):
     chat_id = message['message']['chat']['id']
-    store_id = message['message']['chat']['text']
+    store_id = message['message']['text']
 
-    store_id.replace('/', '')
+    store_id = store_id.replace('/', '')
 
     try:
         store_id = int(store_id)
@@ -79,11 +78,6 @@ def parse_message(message):
         store_id = 'error'
     
     return chat_id, store_id
-
-    # d2 = d1[['Store', 'prediction']].groupby('Store').sum().reset_index()
-
-    # for i in range(len(d2)):
-    #     print('Store Number {} will sell R${:,.2f} in the next 6 weeks'.format(d2.loc[i, 'Store'], d2.loc[i, 'prediction'] ))
 
 # API Initialize
 app = Flask(__name__)
@@ -93,7 +87,7 @@ def index():
     if request.method == 'POST':
         message = request.get_json()
 
-        chat_id, store_id = parse_message()
+        chat_id, store_id = parse_message(message)
 
         if store_id != 'error':
             # loading data
@@ -102,10 +96,12 @@ def index():
             if data != 'error':
                 # prediction
                 d1 = predict(data)
+                
                 # calculation
                 d2 = d1[['Store', 'prediction']].groupby('Store').sum().reset_index()
+                
                 # send message
-                msg = 'Store Number {} will sell €{:,.2f} in the next 6 weeks'.format(d2['Store'].values[0], d2['prediction'].values[0])
+                msg = 'Store Number {} will sell €{:,.2f} - (R${:,.2f}) in the next 6 weeks'.format(d2['Store'].values[0], d2['prediction'].values[0], d2['prediction'].values[0] * 5.63)
                 send_message(chat_id, msg)
                 return Response('OK', status=200)
             else:
@@ -119,4 +115,5 @@ def index():
         return '<h1> Rossmann Telegram Bot </h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
